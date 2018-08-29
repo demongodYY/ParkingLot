e30868600a71551411d797b7a190f8d361ce0ff6
Hill <tenetacid@163.com>
2018-08-29 15:26:04 +0800

    finish define ParkingLot and Car

--- test/oo-practice.spec.js
+++ test/oo-practice.spec.js
@@ -1,5 +1,5 @@
-const Obj = require('../lib/Obj');
-
+const ParkingLot = require('../lib/ParkingLot');
+const Car = require('../lib/Car');
 const chai = require('chai');
 chai.should();
 
@@ -10,6 +10,16 @@ describe('oo-practice', () => {
     });
     it('should be not ok', () => {
       (1).should.be.not.eq(2)
+    });
+  });
+  context('Parking Lot test',() => {
+    it('there is a parking lot',()=> {
+      const parkingLot = new ParkingLot();
+      parkingLot.constructor.should.be.eq(ParkingLot);
+    });
+    it('there should be a car', () => {
+      const car = new Car();
+      car.constructor.should.be.eq(Car);
     });
   });
 })
